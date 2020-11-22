bd6ce6c33e760dc1542f399d12a16d993c01b9f4
nok3zy <nok3zy@gmail.com>
2020-11-22 13:17:25 +0900

    pic_modal

--- public/js/group.js
+++ public/js/group.js
@@ -1,28 +1,34 @@
 class App{
     constructor(){
         this.addBtn = document.querySelector("#addBtn");
-        this.modal = document.querySelector(".modal");
+        this.addModal = document.querySelector(".addBtn_modal");
+        this.picModal = document.querySelector(".pic_modal");
         this.imgs = document.getElementsByTagName("img");
 
         this.addBtn.addEventListener("click",this.clickBtn.bind(this),false);
         window.addEventListener("click",this.clickWindow.bind(this),false);
         
         for(let i =0;i<this.imgs.length;i++){
-            this.imgs[i].addEventListener("click",function(){
-            console.log(`img${i} click`);
-            });
+            this.imgs[i].addEventListener("click",this.clickImg.bind(this),false);
         }
     }
     
+    clickImg(i){
+        console.log(`img${i} click`);
+        this.picModal.style.display="block";
+    }
+
     clickBtn(){
         console.log("btn click");
-        this.modal.style.display="block";
+        this.addModal.style.display="block";
     }
 
     clickWindow(event){
-        if (event.target == this.modal){
+        if (event.target == this.addModal){
             console.log("window click");
-            this.modal.style.display="none";
+            this.addModal.style.display="none";
+        }else if(event.target == this.picModal){
+            this.picModal.style.display="none";
         }
     }
 }
